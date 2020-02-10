a133721c021285e8a3642ef678244e078d8f73f4
lathsamee <giftsforam@gmail.com>
2020-02-10 17:24:07 +0700

    test router react js

--- src/App.js
+++ src/App.js
@@ -5,33 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="Container">
-        <div className="Body">
-          <p>content</p>
-          <p>content</p>
-          <p>content</p>
-          <p>content</p>
-          <p>content</p>
-          <p>content</p>
-          <p>content</p>
-          <p>content</p>
-          <p>content</p>
-          <p>content</p>
-          <p>content</p>
-          <p>content</p>
-          <p>content</p>
-          <p>content</p>
-          <p>content</p>
-          <p>content</p>
-        </div>
-        <div className="Body">
-          <p>content</p>
-        </div>
-        <div className="Body">
-          <p>content</p>
-        </div>
-        <div className="Body">
-          <p>content</p>
-        </div>
+        <p>test</p>
       </div>
     );
   }
---
+++ src/lib/router.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import about from "../screen/layout";
+import user from "../screen/user";
+import home from "../screen/home";
+export default class router extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
