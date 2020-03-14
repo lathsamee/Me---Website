48b2df4b6fd2b59787fbe4d5abdbc89efbc84c6d
lathsamee <giftsforam@gmail.com>
2020-03-14 22:45:27 +0700

    add loading 4 leo

---
+++ src/Exports/Animetions/Loading4.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../../App.css";
+
+const Loading4 = props => {
+  return (
+    <div class="multi-spinner-container">
+      <div class="multi-spinner">
+        <div class="multi-spinner">
+          <div class="multi-spinner">
+            <div class="multi-spinner">
+              <div class="multi-spinner">
+                <div class="multi-spinner"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Loading4.propTypes = {};
+
+export default Loading4;
